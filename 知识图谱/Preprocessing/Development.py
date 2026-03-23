39888c87b01415ef186bac0c0d5ee51cbc2baa05
jrtat <58645976+jrtat@users.noreply.github.com>
2026-03-23 20:34:10 +0800

    Update Development.py

--- 知识图谱/Preprocessing/Development.py
+++ 知识图谱/Preprocessing/Development.py
@@ -13,20 +13,20 @@ def tool_development_coding(company_type, size):
     other_kwd = ['未融资', '不需要融资']           # 未融资等状态
 
     if any(kwd in ct for kwd in soar_kwd):  # 天使轮/A/B轮
-        return 'Soar'
+        return '公司处于天使轮/A轮/B轮，正位于高速上升期'
     elif any(kwd in ct for kwd in rise_kwd):  # C轮往后
-        return 'Rise'
+        return '公司处于C轮/D轮/E轮/F轮，正位于平稳上升期'
     elif any(kwd in ct for kwd in strong_kwd): # 已上市
-        return 'Strong'
+        return '公司已上市，市场认可度高'
     elif any(kwd in ct for kwd in other_kwd) or ct == '': # 未融资 / 不需要融资
         if size == 'E' or size == 'F' or size == 'G': # 只要规模大，就认为是稳定的
-            return 'Stable'
+            return '公司未融资或不需要融资，且公司规模较大，比较稳定'
         elif size != 'H': # 规模小
-            return 'Small'
+            return '公司未融资或不需要融资，且公司规模较小，稳定性尚可'
         else: # 规模未知
-            return 'Unknown'
+            return '公司未融资或不需要融资，公司规模未知，发展情况未知'
     else:
-        return 'Unknown' # ct为空
+        return '公司类型未知，发展情况未知' # ct为空
 
 def preprocess_development(loc_df):
     if '公司类型' not in loc_df.columns:

72791eb6a39db0490c3be72586a733e0726781cd
jrtat <58645976+jrtat@users.noreply.github.com>
2026-03-31 18:43:27 +0800

    Update main.py

--- 知识图谱/main.py
+++ 知识图谱/main.py
@@ -1,9 +1,11 @@
-from func.build_graphRAG import init, RAG_llm, hybrid_retriever
-from func.use_graphRAG import test_search
+from func.build_graphRAG import init
+from func.use_graphRAG import use_llm
+from func.extract_document import get_extracted_document
 
-raw_text = ["这里放你的第一段文本...", "这里放你的第二段文本..."] # 你的原始文本（以后可以换成从文件读取）
-init(raw_text) # 初始化graphRAG后得到 hybrid_retriever
-answer = use_llm(RAG_llm, hybrid_retriever, "熟练使用C++，Java，MySQL适合应聘哪些岗位？") # 提问
+raw_text = get_extracted_document()
+hybrid_retriever = init(raw_text) # 初始化graphRAG后得到 hybrid_retriever
+
+answer = use_llm(hybrid_retriever,"熟练使用C++，Java，MySQL适合应聘哪些岗位？") # 提问
 
 print("=== GraphRAG 的回答 ===")
 print(answer) # 输出回答

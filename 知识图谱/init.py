761eab2c866022bc2d1ac1790b7d372069c6ee1f
jrtat <58645976+jrtat@users.noreply.github.com>
2026-04-01 00:47:20 +0800

    Update init.py

--- 知识图谱/init.py
+++ 知识图谱/init.py
@@ -1,5 +1,4 @@
-from func.build_graphRAG import init, get_retriever
-from func.use_graphRAG import use_llm
+from func.build_graphRAG import init
 from func.extract_document import get_extracted_document
 
 raw_text = get_extracted_document()

61a75401dcd5f8f960191cd592356dbe1b4bcc7b
jrtat <58645976+jrtat@users.noreply.github.com>
2026-03-31 21:49:32 +0800

    Update use_graphRAG.py

--- 知识图谱/func/use_graphRAG.py
+++ 知识图谱/func/use_graphRAG.py
@@ -3,6 +3,22 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 
+def get_llm():
+    return ChatOpenAI(
+        model="qwen3:8b",  # 模型名字（xjx实验室）
+        base_url="http://59.72.63.156:14138/v1", # url（xjx实验室）
+        api_key="EMPTY",  # vLLM 不需要真实 key
+        temperature=0  # 温度0 = 输出最稳定（对于提取图谱这个应用来说，0是最好的，不要调这个参数）
+    )
+
+def get_llm_temp():
+    return ChatOpenAI(
+        model = "Qwen2.5-3B",  # 模型名字（本地）
+        base_url="http://127.0.0.1:8000/v1",  # url本地
+        api_key="EMPTY",  # vLLM 不需要真实 key
+        temperature=0  # 温度0 = 输出最稳定（对于提取图谱这个应用来说，0是最好的，不要调这个参数）
+    )
+
 def use_llm(hybrid_retriever,question: str):
 
     # Step 0：判断graphRAG是否已经构建完成了
@@ -10,13 +26,8 @@ def use_llm(hybrid_retriever,question: str):
         raise ValueError("请先调用 init() 初始化！")
 
     # Step 1：声明大模型
-    llm = ChatOpenAI(
-        model="Qwen2.5-3B",  # 你在 vLLM 里部署的模型名字
-        base_url="http://127.0.0.1:8000/v1",  # vLLM 的 OpenAI 兼容接口
-        api_key="EMPTY",  # vLLM 不需要真实 key
-        temperature=0  # 温度0 = 输出最稳定（提取图谱必须这样）
-    )
-
+    llm = get_llm_temp()
+    
     # Step 2：构建 RAG Chain
     template = """
     你是一个专业、准确的助手。请严格基于以下上下文回答问题。

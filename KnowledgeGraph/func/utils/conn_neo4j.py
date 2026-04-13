3ec1b63c69f52e6310d6bf33345851e678656c23
oQAQo233 <346601429@qq.com>
2026-04-14 00:00:06 +0800

    使用ls的数据库

--- KnowledgeGraph/func/utils/conn_neo4j.py
+++ KnowledgeGraph/func/utils/conn_neo4j.py
@@ -1,16 +1,16 @@
 from langchain_community.graphs import Neo4jGraph
 
 #--- 全局变量 ---#
-# database_name = "bde42e4c"
-# graph_url = "neo4j+s://bde42e4c.databases.neo4j.io"
-# graph_username = "bde42e4c"
-# graph_password = "Bj11oN-jZzUGJ47resfaMFvqZBxX7FE2JiUnZSBzcPY"
+database_name = "bde42e4c"
+graph_url = "neo4j+ssc://bde42e4c.databases.neo4j.io"
+graph_username = "bde42e4c"
+graph_password = "Bj11oN-jZzUGJ47resfaMFvqZBxX7FE2JiUnZSBzcPY"
 
 #--- 全局变量 ---#
-database_name = "1f5dcc17"
-graph_url = "neo4j+s://1f5dcc17.databases.neo4j.io"
-graph_username = "1f5dcc17"
-graph_password = "J1-Sv2VA6q5Qw3rH5vFQSmYCwMtuCpF8kqt-W0UrEDU"
+# database_name = "1f5dcc17"
+# graph_url = "neo4j+ssc://1f5dcc17.databases.neo4j.io"
+# graph_username = "1f5dcc17"
+# graph_password = "J1-Sv2VA6q5Qw3rH5vFQSmYCwMtuCpF8kqt-W0UrEDU"
 
 def connect_neo4j():
     """

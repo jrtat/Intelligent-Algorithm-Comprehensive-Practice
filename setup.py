1bd0441f6b1380589260e1001979f419786268ca
oQAQo233 <346601429@qq.com>
2026-04-14 10:47:49 +0800

    添加vscode安装配置

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+# 重复三次分别为三个模块进行打包
+
+# setup(
+#     name='KnowledgeGraph',
+#     packages=find_packages(),
+#     version='1.0.0'
+# )
+
+# setup(
+#     name='processor',
+#     packages=find_packages(),
+#     version='1.0.0'
+# )
+
+setup(
+    name='RelationGraph',
+    packages=find_packages(),
+    version='1.0.0'
+)

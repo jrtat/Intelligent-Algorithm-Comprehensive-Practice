4b08666e6e9d2bc07bd4cb8f02a130d8c8634316
xuejx <346601429@qq.com>
2026-04-14 07:03:08 +0000

    优化setup

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-# 重复三次分别为三个模块进行打包
+# 重复三次分别为三个模块进行打包 pip install -e .
 
 # setup(
 #     name='KnowledgeGraph',
-#     packages=find_packages(),
+#     packages=['KnowledgeGraph'],
 #     version='1.0.0'
 # )
 
 # setup(
 #     name='processor',
-#     packages=find_packages(),
+#     packages=['processor'],
 #     version='1.0.0'
 # )
 
 setup(
     name='RelationGraph',
-    packages=find_packages(),
+    packages=['RelationGraph'],
     version='1.0.0'
 )

5a5fcc993960d97e4998500dcd29ae91dc49e926
oQAQo233 <346601429@qq.com>
2026-05-04 16:48:41 +0800

    大改结构

--- src/RelationGraph/func/utils/config.py
+++ src/RelationGraph/func/utils/config.py
@@ -4,9 +4,17 @@ dataset_path = "func/model/lora/dataset_raw_ver2/job_classify_dataset"
 model_path = "func/model/lora/model_ver3/lora_job_classifier"
 
 model_name = "hfl/chinese-macbert-large"
+
+# dataset_path = "RelationGraph/func/model/lora/dataset_raw_ver2/job_classify_dataset"
+# model_path = "RelationGraph/func/model/lora/model_ver3/lora_job_classifier"
+# model_name = "hfl/chinese-macbert-large"
+
 # hfl/chinese-macbert-large
 # hfl/chinese-macbert-base
 
 personal_model_path1 = "./func/model/mlp/model_ver1/mlp_model.pt"
 personal_model_path2 = "./func/model/mlp/model_ver2/mlp_model.pt"
+
+# personal_model_path1 = "./RelationGraph/func/model/mlp/model_ver1/mlp_model.pt"
+# personal_model_path2 = "./RelationGraph/func/model/mlp/model_ver2/mlp_model.pt"
 

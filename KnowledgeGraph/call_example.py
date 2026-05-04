dea72d0eab790f68cf5f516bbe6d94acf8d9a3d9
jrtat <58645976+jrtat@users.noreply.github.com>
2026-05-04 15:05:22 +0800

    Delete KnowledgeGraph/call_example.py

--- KnowledgeGraph/call_example.py
+++
@@ -1,7 +0,0 @@
-from KnowledgeGraph.func.use_graph.chyper_search import get_node_id_by_value_and_label,get_related_node_ids
-
-# print(get_job_property_merge_vals(1880,"学历要求"))
-id = get_node_id_by_value_and_label("统计员","职业类别","id")
-print(id)
-print(get_related_node_ids(id, "属于"))
-print(len(get_related_node_ids(id, "属于")))
